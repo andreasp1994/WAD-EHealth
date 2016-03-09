916a2385ce687b6cf488dd090de9430c3855ded2
2079310R <2079310R@student.gla.ac.uk>
2016-03-09 10:58:26 +0000

    Results page started

--- ehealthexplorer/explorer/urls.py
+++ ehealthexplorer/explorer/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 from explorer import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+        url(r'^$', views.index, name='index'),
+        url(r'^results/$', views.results, name='results'),
+        )
--- ehealthexplorer/explorer/views.py
+++ ehealthexplorer/explorer/views.py
@@ -8,3 +8,8 @@ def index(request):
     response = render(request,'explorer/index.html', context_dict)
 
     return response
+    
+def results(request):
+    context_dict={'results':[{'title':'google','url':'http://www.google.co.uk'}]}   ## Placeholder until search function
+    response = render(request,'explorer/results.html',context_dict)                 ## can be used.
+    return response
