c6b481736209cd575a98c8b18d58de973afc5496
npitsillos <npitsillos@hotmail.com>
2016-03-11 17:15:43 +0000

    register form drop menu

--- ehealthexplorer/ehealthexplorer/urls.py
+++ ehealthexplorer/ehealthexplorer/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^explorer/', include('explorer.urls')), # ADD THIS NEW TUPLE!
+    url(r'^explorer/', include('explorer.urls')), #
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 )
