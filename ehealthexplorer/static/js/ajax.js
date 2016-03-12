c8a22ce803e820999732a6d2ec635e1563d41891
Andreas Pogiatzis <andreasp1994@gmail.com>
2016-03-12 18:14:49 +0200

    register dyanamic switch

--- ehealthexplorer/static/js/ajax.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.main-menu li a').click(function(e){
-        $.get($(e.target).data('link')  , function(data) {
-            temp = data;
-            $('#sidebar').html(temp);
-        });
-    });
-});
--- ehealthexplorer/static/js/input-script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-
-    $('.tab-pane li').click(function(e){
-        $("#search_box").val($("#search_box").val() + $(e.target).text() + " ");
-    }
-    );
-
-});
