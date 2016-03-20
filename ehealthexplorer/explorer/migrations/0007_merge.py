d425884d7af292f592c272b0c2c577f05e7fb23a
npitsillos <npitsillos@hotmail.com>
2016-03-20 19:08:50 +0000

    fixes

---
+++ ehealthexplorer/explorer/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('explorer', '0004_merge'),
+        ('explorer', '0006_merge'),
+    ]
+
+    operations = [
+    ]
